98b368dbd3a56f6aa24f0af8bdba22c547f7385d
omidmaldar <maldar@gmail.com>
2017-02-09 13:01:58 +0200

    create a app/tmp folder

---
+++ tmp/second.js
@@ -0,0 +1,24 @@
+/**
+ * Created by omid on 2/9/17.
+ */
+
+const express = require('express')
+const app = express()
+
+app.use(function(request, response, next)  {
+    console.log(request.headers)
+next()
+})
+
+app.use(function (request, response, next)  {
+    request.chance = Math.random()
+next()
+})
+
+app.get('/middleWare', function (request, response) {
+    response.json({
+    chance: request.chance
+})
+})
+
+app.listen(3000)
